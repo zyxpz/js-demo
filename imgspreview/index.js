d5366e056b2e9ff0852de5ed8849cf77c686b254
wb-mxl231031 <wb-mxl231031@cainiao.com>
2017-12-27 12:36:11 +0800

    add: 桌面版点击转换事件

--- imgspreview/index.js
+++ imgspreview/index.js
@@ -1,18 +1,18 @@
 class ImgsPreviewPopup {
   init() {
-    this.overlay = $('<div class="imgs-preview-popup"></div>');
+    this.overlay = $(`<div class="imgs-preview-popup"></div>`);
     this.slider = $('<div class="imgs-slide"></div>');
     this.prevArrow = $('<a class="prevArrow"></a>');
     this.nextArrow = $('<a class="nextArrow"></a>');
     this.pageSpan = $('<span class="imgs-pages"></span>');
     this.close = $('<span class="imgs-close J-close">&#10005;</span>');
-    this.placeholders = $('<div class="placeholder"></div>');
+    this.placeholders = $([]);
   }
   handleEvent() {
     this.overlay.on('click', '.J-close', this.handleHide.bind(this));
   }
   handleCreatePopupHtml() {
-    this.overlay.appendTo('body');
+    this.overlay.appendTo('#popup');
     this.slider.appendTo(this.overlay);
     this.pageSpan.appendTo(this.overlay);
     this.close.appendTo(this.overlay);
@@ -22,6 +22,7 @@ class ImgsPreviewPopup {
   }
   handleHide() {
     this.overlay.removeClass('visible');
+    this.placeholders = $([]);
     this.overlay.remove();
   }
 }
@@ -44,6 +45,8 @@ class ImgsPreview extends ImgsPreviewPopup {
   init() {
     this.warp = $(this.attrs.warp);
 
+    this.ifOnTouch();
+
     this.handleEvent();
   }
 
@@ -55,12 +58,12 @@ class ImgsPreview extends ImgsPreviewPopup {
       // 触发动画
       this.overlay.addClass('visible');
     }, 100);
-
-    this.pageSpan.text(`${this.index + 1}/${this.imgLen}`);
   }
 
   handleEvent() {
     this.warp.on('click', `${this.attrs.imgThumbs}`, this.querIndex.bind(this));
+    this.overlay.on('click', '.prevArrow', this.prevArrowClick.bind(this));
+    this.overlay.on('click', '.nextArrow', this.nextArrowClick.bind(this));
   }
 
   querIndex(e) {
@@ -74,22 +77,48 @@ class ImgsPreview extends ImgsPreviewPopup {
 
     this.imgLen = this.imgs.length;
 
-    // this.imgs.each(() => {
-    //   this.placeholders = this.placeholders.add($('<div class="placeholder"></div>'));
-    // });
+    this.imgs.forEach((e) => {
+      this.placeholders = this.placeholders.add($(`<div class="placeholder" data-index="${$(e).index()}"><img src="${$(e).attr('href')}"/></div>`));
+    });
 
-    this.slider.append(this.placeholders);
+    this.slider.html(this.placeholders);
 
-    this.imgHtml(this.image);
+    this.imgShowIndex(this.index);
 
     this.createHtml();
 
   }
 
-  imgHtml(src) {
-    const img = $('<img/>')
-    img.attr('src', src);
-    this.placeholders.html(img);
+  imgShowIndex(index) {
+    this.slider.css({
+      left: `${ - index * 100 }%`
+    });
+    this.pageSpan.text(`${this.index + 1}/${this.imgLen}`);
   }
+
+  ifOnTouch() {
+    if (!("ontouchstart" in window)) {
+      this.overlay.append(this.prevArrow).append(this.nextArrow);
+    }
+  }
+
+  prevArrowClick() {
+    this.index = this.index - 1;
+    if (this.index < 0) {
+      this.index = 0;
+      return;
+    }
+    this.imgShowIndex(this.index);
+  }
+
+  nextArrowClick() {
+    this.index = this.index + 1;
+    if (this.index >= this.imgLen) {
+      this.index = this.imgLen - 1;
+      return;
+    }
+    this.imgShowIndex(this.index);
+  }
+
 
 }
